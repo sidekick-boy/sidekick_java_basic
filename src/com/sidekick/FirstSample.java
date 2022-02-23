959dda8abf5ed3c6b260e3f4c33c9d79f7a53171
sidekick-boy <wsdahstu@163.com>
2022-02-23 17:55:00 +0800

    提交知识管理文档和图片资源

--- src/com/sidekick/FirstSample.java
+++ src/com/sidekick/FirstSample.java
@@ -1,7 +1,34 @@
 package com.sidekick;
 
+import java.util.Scanner;
+
 public class FirstSample {
     public static void main(String[] args) {
-        System.out.println("we will no use 'Hello World!'");
+/*        System.out.println("we will no use 'Hello World!'");
+
+        int x = 10;
+        double str = 10.1;
+        double y = Double.NaN;
+        double m = Double.POSITIVE_INFINITY;
+        double n = Double.NEGATIVE_INFINITY;
+
+        System.out.println(Double.isNaN(x));
+        System.out.println(Double.isNaN(str));
+        System.out.println(y);
+        System.out.println(m);
+        System.out.println(n);*/
+
+/*        double x = 4;
+        System.out.println(Math.sqrt(x));
+
+        System.out.println(1<<35);*/
+        // 读取输入
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("我是谁？" + name);
+        String where = in.nextLine();
+        System.out.println("我在哪？" + where);
+        String what = in.nextLine();
+        System.out.println("我在干什么？" + what);
     }
 }
---
+++ src/com/sidekick/Retirement.java
@@ -0,0 +1,13 @@
+package com.sidekick;
+
+import java.util.Scanner;
+
+/**
+ * @author sidekick-boy
+ * @email sidekick_boy@outlook.com
+ * @date 2022/2/24 12:51
+ **/
+public class Retirement {
+    // read inputs
+    Scanner in = new Scanner(System.in);
+}
